9995232444608b96f417294112d1f717393498f9
tomashchuk89 <oleksandr.tomashchuk89@gmail.com>
2022-06-12 20:55:56 +0300

    commit hw2

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+let num = 28;
+
+console.log(num.toString(16));
+console.log(num.toString(2));
+
+
+
+const x = prompt('Number entry');
+
+console.log(+x + 2);
+console.log(x - 2);
+console.log(x * 2);
+console.log(x / 2);
+
+console.log(x.length);
+
+
+
+
+let number1 = 0.51000002;
+    number2 = number1.toFixed(2);
+
+console.log(+number2 + 2);
+
+
+
+const str = 'the quick brown fox jumps over the lazy dogs back';
+const str_two = 'brown fox jumps';
+
+console.log(str.indexOf(str_two));
+console.log(str_two.toUpperCase());
+
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 3;
+
+
+console.log((a<c, c>b) || (b<c,c>c) || (c>c, c==d));
+console.log((a<c, c>b) && (b<c, c==c) && (c==c, c==d));
+
+
+
+let number3 = 0;
+let number4 = "0";
+let y = false;
+
+console.log((number3 == number4)==y);
+console.log((number3 === number4)===y);
+
+
+
+
+
