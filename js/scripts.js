716ed7c8e8b3ae6c5f5b43245c3ab6cb089a3805
kateryna.tomashchuk <kateryna.tomashchuk@prosapient.com>
2022-06-18 12:57:50 +0300

    HW_3

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,61 @@
-let num = 28;
-console.log(num.toString(16));
-console.log(num.toString(2));
+//#1
+let names = prompt('Input your name?');
+let str = ''; 
+for (i = 1; i <= 4; i++) {
+    if(i===3) str +='Happy birthday, dear ' +  names + '\n';
+    else str += 'Happy birthday to you \n';
+}
+console.log(str); 
+//#2
+let number = prompt('Input number 2');
+let result1 = "";
+  for (let i = 0; i < number; i++) {
+    result1 += ".#";
+  }
+  console.log(result1);
 
-const x = prompt('Number entry');
-console.log(+x + 2);
-console.log(x - 2);
-console.log(x * 2);
-console.log(x / 2);
-console.log(x.length);
+ //#3
+  let number2 = prompt ('Input number 3');
+  let sum = 0;
+  if (!number2) {
+    number2 = prompt('Invalid. You should enter a number');
+  }
+  for (let i = 0; i < number2; i++) {
+    if (i % 2 != 0) {
+      sum += i;
+    }
+  }
+  console.log(sum);
+ //#4
+  let count = 0;
+  let breaker;
+  while (true) {
+    count += 1;
+    breaker = Math.random();
+    if (breaker > 0.7) {
+      break;
+    }
+  }
+  console.log(`Loop was finished because of: ${breaker} \nNumber of iterations: ${count}`);
+ //#5
+  for (let i = 1; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("Div by 5 and 3");
+    } else if (i % 3 == 0) {
+      console.log("Div by 3");
+    } else if (i % 5 == 0) {
+      console.log("Div by 5");
+    } else {
+      console.log(i);
+    }
+  }
+//#6
+var date = new Date();
 
-let number1 = 0.51000002;
-    number2 = number1.toFixed(2);
-console.log(+number2 + 2);
-
-const str = 'the quick brown fox jumps over the lazy dogs back';
-const str_two = 'brown fox jumps';
-console.log(str.indexOf(str_two));
-console.log(str_two.toUpperCase());
-
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 3;
-console.log((a<c, c>b) || (b<c,c>c) || (c>c, c==d));
-console.log((a<c, c>b) && (b<c, c==c) && (c==c, c==d));
-
-let number3 = 0;
-let number4 = "0";
-let y = false;
-console.log((number3 == number4)==y);
-console.log((number3 === number4)===y);
-
-let operator = 'output on display';
-let operator1 = undefined;
-let operator2 = null;
-let operator3 = operator ?? operator1 ?? operator2;
-console.log(operator3);
-
-
-
+function toLocal (date) {
+	var local = new Date(date);
+	local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+	return local.toJSON();
+}
+console.log(toLocal(date));
 
